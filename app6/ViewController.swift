54c11e6a299c7469f779779d657b0b8627dd29ff
Gambster <gamboa040200@gmail.com>
2023-09-14 15:26:21 -0700

    Initial Commit

---
+++ app6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app6
+//
+//  Created by Tecsom on 14/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
